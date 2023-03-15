d6b2a322e1cbf99b8ad45c2e9c6775ea7aa25c30
jade alfaiao <jade.alfaiao@gmail.com>
2023-03-15 16:27:45 +0100

    style

--- src/components/NewRecipe.vue
+++ src/components/NewRecipe.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
         Add a new recipe
+        <br>
+        <br>
       <div @keyup.enter="$emit('create-new-recipe', recipe)">
         <input type="text" placeholder="Enter a recipe name" v-model="recipe.newRecipeTitle" />
         <input type="text" placeholder="Enter a recipe description" v-model="recipe.newRecipeDesc" />
       </div>
+      <br>
       <button @click="$emit('create-new-recipe', recipe)">Add</button>
     </div>
   </template>
